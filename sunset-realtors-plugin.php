bf32314635059f6746871f3264fe649c07654a3b
thomasbulten <thomasbulten7@gmail.com>
2026-07-09 11:00:53 +0200

    New version 0.1.2

--- sunset-realtors-plugin.php
+++ sunset-realtors-plugin.php
@@ -7,7 +7,7 @@
  * Author: TAB IT Development
  * Author URI: https://tabitdevelopment.nl
  * Text Domain: sunset-realtors-plugin
- * Version: 0.1.1
+ * Version: 0.1.2
  *
  * @package TAB\Sunset_Realtors
  */
@@ -16,7 +16,7 @@ declare(strict_types=1);
 
 namespace TAB\Sunset_Realtors;
 
-define('SUNSET_REALTORS_PLUGIN_VERSION', '0.1.1');
+define('SUNSET_REALTORS_PLUGIN_VERSION', '0.1.2');
 define('SUNSET_REALTORS_PLUGIN_DOMAIN', 'sunset-realtors-plugin');
 define('SUNSET_REALTORS_PLUGIN_DIR_PATH', untrailingslashit(plugin_dir_path(__FILE__)));
 define('SUNSET_REALTORS_PLUGIN_DIR_URL', untrailingslashit(plugin_dir_url(__FILE__)));
